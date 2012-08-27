1af038c73927c6f2f67671e4dcad24be3acdf722
devrunata <atanur-oeztuerk@gmx.de>
2012-08-27 17:16:02 +0200

    finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,12 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+    
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',:text =>"Sample App | Home")
+    end
+    
   end
   
   describe "Help page" do
@@ -15,6 +21,12 @@ describe "Static pages" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
+    
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',:text =>"Sample App | Help")
+    end
+    
   end
   
   describe "About page"do
@@ -22,6 +34,12 @@ describe "Static pages" do
       visit '/static_pages/about'
       page.should have_content('About')
     end
+    
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',:text =>"Sample App | About")
+    end
+    
   end
   
 end
