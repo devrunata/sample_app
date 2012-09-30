635a8a3e237d23e3f00f1d59bcefb95ba2720039
devrunata <atanur-oeztuerk@gmx.de>
2012-09-30 18:32:05 +0200

    finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,13 @@
 #
 
 
-
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   
   
   before_save{ |user| user.email=email.downcase }
+  before_save :create_remember_token
   
   validates   :name,      :presence=>true, :length=>{ :maximum=> 50 }
   VALID_EMAIL_REGEX=/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -27,4 +27,9 @@ class User < ActiveRecord::Base
   validates   :password_confirmation, :presence=>true   
   
   
+  
+  private  
+    def create_remember_token
+      self.remember_token = SecureRandom.base64.tr('+/', '-_').tr('=', '')
+    end 
 end
